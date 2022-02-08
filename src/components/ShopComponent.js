cff677162f565acf6587311c43461b7565559eee
David Dukelow <dwduke80@gmail.com>
2022-02-07 22:12:20 -0800

    some work on shop hover, but not working yet

--- src/components/ShopComponent.js
+++ src/components/ShopComponent.js
@@ -4,10 +4,27 @@ import React, { Component } from 'react';
 class Shop extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      imgOpacity: 1,
+      textOpacity: 0
+    };
     this.shopHover = this.shopHover.bind(this);
+    this.shopUnhover = this.shopUnhover.bind(this);
   }
   
-  shopHover()
+  shopHover() { 
+    this.setState({
+      imgOpacity: 0.5,
+      textOpacity: 1
+    })
+  }
+
+  shopUnhover() { 
+    this.setState({
+      imgOpacity: 1,
+      textOpacity: 0
+    })
+  }
 
   shopItem(shop) {
     return(
@@ -15,9 +32,16 @@ class Shop extends Component {
         <div className="row mb-5">
           {shop.map(shop =>
             <div className="col-md-4" key={shop.id}>
-                <a class="btn shop-8Bit-btn" onMouseEnter={() => {console.log("Event:MouseEnter");}} href="#" role="button">
-                    <img src={require(`../../public/assets/images/${ shop.imgSrc }.jpg`)} width="100%" alt={shop.alt} />
-                    <p class="shop-txt">{shop.description}<br />{shop.price}</p>
+                <a class="btn shop-8Bit-btn" 
+                  onMouseEnter={() => {this.shopHover();}} 
+                  onMouseLeave={() => {this.shopUnhover();}} 
+                  href="#" 
+                  role="button">
+                    <img src={require(`../../public/assets/images/${ shop.imgSrc }.jpg`)} 
+                      style={{ opacity:this.state.imgOpacity }}
+                      width="100%" 
+                      alt={shop.alt} />
+                    <p class="shop-txt" style={{ opacity:this.state.textOpacity }}>{shop.description}<br />{shop.price}</p>
                 </a>
                 <p class="d-md-none txt-120">{shop.description}<br />{shop.price}</p>
             </div>
